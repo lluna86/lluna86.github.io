04111cf663fae8024d2afbdce750baa093812176
Mutsuda <mutsuda@gmail.com>
2023-01-13 23:34:33 +0100

    added js

---
+++ js/init.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('#nav-toggle').on("click", function() {
+    $('#nav-menu')[0].classList.toggle('hidden')
+    $('#hamburger')[0].classList.toggle('hidden')
+    $('#cross')[0].classList.toggle('hidden')
+  });  
+});
